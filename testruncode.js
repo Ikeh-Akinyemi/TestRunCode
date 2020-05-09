641dc1f5bf5bae0cba1069d2bd7f8e27638f0b50
Ikeh-Akinyemi <mrikehchukwuka@gmail.com>
2020-05-09 05:15:14 +0100

    Updating the js file

---
+++ testruncode.js
@@ -0,0 +1,18 @@
+/*var yourAge = prompt("What is your Age");
+var outPut = yourAge * 365;
+console.log(yourAge);
+alert(outPut);
+/*var myFirstName = prompt("What is your first name");
+var myLastName = prompt("what is your last name");
+var myAge = prompt("what is your age");
+alert("Welcome, " + myFirstName + " " + myLastName);
+alert("Your age is " + myAge);
+console.log("from the script file");*/
+function jess(curly) {
+    if (curly) {
+      return "it's curly";
+    }
+    return "it's not curly";
+  }
+  
+  console.log(jess(false));
